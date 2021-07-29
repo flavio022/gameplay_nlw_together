a7791b0e9bcd0994a93810dd980248c5647d8fee
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-07-29 12:20:53 -0300

    adjusting screen signIn

--- src/screen/SignIn/index.tsx
+++ src/screen/SignIn/index.tsx
@@ -24,9 +24,9 @@ export function SignIn() {
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas{`\n`}
-                    facilmentes</Text>
+                    Conecte-se {`\n`}
+                    e organize suas{`\n`}
+                    jogatinas</Text>
 
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games {`\n`}
